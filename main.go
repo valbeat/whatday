6210911b974c4377d7f196331f2439e8a75d8e99
valbeat <kj1ktk@gmail.com>
2018-05-02 01:33:11 +0900

    wip

---
+++ main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func getListBody() ([]byte, error) {
+	now := time.Now()
+	m := int(now.Month())
+	d := int(now.Day())
+	cacheName := "list_" + strconv.Itoa(m) + "-" + strconv.Itoa(d) + ".html"
+	f, _ := os.Open(cacheName)
+	defer f.Close()
+	if f != nil {
+		var b []byte
+		buf := make([]byte, 10)
+		for {
+			n, err := f.Read(buf)
+			if n == 0 {
+				break
+			}
+			if err != nil {
+				break
+			}
+			b = append(b, buf[:n]...)
+		}
+		return b, nil
+	}
+	endpoint := "http://www.kinenbi.gr.jp/"
+
+	values := url.Values{}
+	values.Add("M", strconv.Itoa(m))
+	values.Add("D", strconv.Itoa(d))
+
+	fmt.Println(values.Encode())
+	res, err := http.PostForm(endpoint, values)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	f, _ = os.Create(cacheName)
+	defer f.Close()
+	f.Write(body)
+	return body, nil
+}
+
+func getDetailBody(path string) ([]byte, error) {
+	cacheName := "detail_" + path + ".html"
+	f, _ := os.Open(cacheName)
+	defer f.Close()
+	if f != nil {
+		var b []byte
+		buf := make([]byte, 10)
+		for {
+			n, err := f.Read(buf)
+			if n == 0 {
+				break
+			}
+			if err != nil {
+				break
+			}
+			b = append(b, buf[:n]...)
+		}
+		return b, nil
+	}
+
+	base := "http://www.kinenbi.gr.jp/"
+	req := base + path
+	res, err := http.Get(req)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	f, _ = os.Create(cacheName)
+	defer f.Close()
+	f.Write(body)
+	return body, nil
+}
+
+// Article is
+type Article struct {
+	Title string
+	Text  string
+}
+
+func getArticle(s *goquery.Selection) Article {
+	href, _ := s.Attr("href")
+	detail, _ := getDetailBody(href)
+	_doc, _ := goquery.NewDocumentFromReader(strings.NewReader(string(detail)))
+
+	article := Article{}
+	article.Title = strings.TrimSpace(_doc.Find("tr").First().Text())
+	article.Text = strings.TrimSpace(_doc.Find("tr").Last().Text())
+	return article
+}
+
+func main() {
+
+	b, _ := getListBody()
+
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(b)))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	nodeList := doc.Find(".today_kinenbilist .winDetail")
+
+	rand.Seed(time.Now().UnixNano())
+	node := nodeList.Eq(rand.Intn(nodeList.Length()))
+	article := getArticle(node)
+	println(article.Title)
+	println(article.Text)
+}
