60a44da115bedab3482388bb3509fd1e6b9258af
valbeat <kj1ktk@gmail.com>
2018-05-28 00:38:41 +0900

    [clean] unused code

--- example/main.go
+++ example/main.go
@@ -18,5 +18,5 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	i := rand.Intn(articles.Length())
 	article := articles.Articles[i]
-	fmt.Println(article.String())
+	fmt.Printf("## %s\n> %s", article.Title, article.Text)
 }
