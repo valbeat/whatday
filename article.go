6c973e5e96716880ed6f50f3e3eddb8e3792c241
valbeat <kj1ktk@gmail.com>
2018-05-28 00:53:36 +0900

    [update] use decoder

--- article.go
+++ article.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"strings"
 	"time"
 
@@ -30,34 +29,6 @@ type Article struct {
 
 // NewArticles return Articles
 func NewArticles(t time.Time) (*Articles, error) {
-	b, err := getList(t)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-
-	doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(b)))
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-
-	var articles []Article
-
-	nodeList := doc.Find(".today_kinenbilist .winDetail")
-	nodeList.Each(func(i int, node *goquery.Selection) {
-		href, _ := node.Attr("href")
-		article, _ := newArticle(href)
-		articles = append(articles, *article)
-	})
-
-	return &Articles{
-		Date:     t,
-		Articles: articles,
-	}, nil
-}
-
-func getList(now time.Time) ([]byte, error) {
 	cli, err := NewClient("http://www.kinenbi.gr.jp/")
 	if err != nil {
 		fmt.Println(err)
@@ -67,20 +38,19 @@ func getList(now time.Time) ([]byte, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 
-	res, err := cli.GetList(ctx, now)
+	res, err := cli.GetList(ctx, t)
 	if err != nil {
 		fmt.Println(err)
 		return nil, err
 	}
 	defer res.Body.Close()
 
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
+	articles := decodeArticles(res.Body)
 
-	return body, nil
+	return &Articles{
+		Date:     t,
+		Articles: articles,
+	}, nil
 }
 
 func newArticle(spath string) (*Article, error) {
@@ -101,6 +71,20 @@ func newArticle(spath string) (*Article, error) {
 	defer res.Body.Close()
 	article := decodeArticle(res.Body)
 	return article, nil
+}
+
+func decodeArticles(body io.Reader) []Article {
+
+	doc, _ := goquery.NewDocumentFromReader(body)
+
+	var articles []Article
+	nodeList := doc.Find(".today_kinenbilist .winDetail")
+	nodeList.Each(func(i int, node *goquery.Selection) {
+		href, _ := node.Attr("href")
+		article, _ := newArticle(href)
+		articles = append(articles, *article)
+	})
+	return articles
 }
 
 func decodeArticle(body io.Reader) *Article {
