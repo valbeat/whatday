6bdf7d7abca910732a43344e55fa547a416d97bc
valbeat <kj1ktk@gmail.com>
2018-05-27 22:00:44 +0900

    [clean] Move related functions

--- article.go
+++ article.go
@@ -1,7 +1,9 @@
 package whatday
 
 import (
+	"context"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"strings"
 	"time"
@@ -29,6 +31,58 @@ func NewArticle(t time.Time) Article {
 	href, _ := node.Attr("href")
 	article := getArticle(href)
 	return article
+}
+
+func GetListBody(now time.Time) ([]byte, error) {
+	cli, err := NewClient("http://www.kinenbi.gr.jp/")
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	res, err := cli.GetList(ctx, now)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func GetDetailBody(spath string) ([]byte, error) {
+	cli, err := NewClient("http://www.kinenbi.gr.jp/" + spath)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	res, err := cli.GetDetail(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return body, nil
 }
 
 func getArticle(href string) Article {
--- whatday_today.go
+++ whatday_today.go
@@ -1,63 +1,8 @@
 package whatday
 
 import (
-	"context"
-	"fmt"
-	"io/ioutil"
 	"time"
 )
-
-func GetListBody(now time.Time) ([]byte, error) {
-	cli, err := NewClient("http://www.kinenbi.gr.jp/")
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-	defer cancel()
-
-	res, err := cli.GetList(ctx, now)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-
-	return body, nil
-}
-
-func GetDetailBody(spath string) ([]byte, error) {
-	cli, err := NewClient("http://www.kinenbi.gr.jp/" + spath)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-	defer cancel()
-
-	res, err := cli.GetDetail(ctx)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-
-	return body, nil
-}
 
 func Print() {
 	article := NewArticle(time.Now())
