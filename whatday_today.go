5ed52cd7f546884fe3f8385625279a21602a7956
valbeat <kj1ktk@gmail.com>
2018-05-27 21:51:26 +0900

    [clean] extract cache.go

--- whatday_today.go
+++ whatday_today.go
@@ -4,12 +4,8 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
-	"os"
-	"strconv"
 	"time"
 )
-
-const cacheDir string = "./cache/"
 
 func GetListBody(now time.Time) ([]byte, error) {
 	cli, err := NewClient("http://www.kinenbi.gr.jp/")
@@ -61,39 +57,6 @@ func GetDetailBody(spath string) ([]byte, error) {
 	}
 
 	return body, nil
-}
-
-func GetListBodyByCache(now time.Time) ([]byte, error) {
-	m := int(now.Month())
-	d := int(now.Day())
-	cacheName := cacheDir + "list_" + strconv.Itoa(m) + "-" + strconv.Itoa(d) + ".html"
-	return GetCacheBody(cacheName)
-}
-
-func GetDetailByCache(path string) ([]byte, error) {
-	cacheName := cacheDir + "detail_" + path + ".html"
-	return GetCacheBody(cacheName)
-}
-
-func GetCacheBody(cacheName string) ([]byte, error) {
-	f, _ := os.Open(cacheName)
-	defer f.Close()
-	if f == nil {
-		return nil, nil
-	}
-	var b []byte
-	buf := make([]byte, 10)
-	for {
-		n, err := f.Read(buf)
-		if n == 0 {
-			break
-		}
-		if err != nil {
-			break
-		}
-		b = append(b, buf[:n]...)
-	}
-	return b, nil
 }
 
 func Print() {
