f665f5466b8440b982371655de38c91a36683ab5
valbeat <kj1ktk@gmail.com>
2018-06-21 00:28:05 +0900

    Fix test case to empty check only.

--- article_test.go
+++ article_test.go
@@ -15,9 +15,7 @@ func TestNewArticles(t *testing.T) {
 
 	i := rand.Intn(len(articles))
 	got := articles[i]
-	// ref: http://www.kinenbi.gr.jp/
-	want := Article{Title: "合格の日", Text: "福岡県福岡市に本社を置き、全国、海外に店舗を展開する天然とんこつラーメン専門店の株式会社一蘭が制定。同社では福岡県太宰府市の太宰府参道店で「合格ラーメン」を提供していることから、入学や資格試験などを受ける受験生を応援するのが目的。日付は５と９で「合（５）格（９）」と読む語呂合わせから。「合格ラーメン」は五角形の器に長さ５９センチの麺が入っているなど、合格（ごうかく）にこだわった内容が人気。"}
-	if got != want {
+	if got.Title == "" || got.Text == "" {
 		t.Errorf("NewArticle is invalid %v", got)
 	}
 }
