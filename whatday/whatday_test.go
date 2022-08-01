d473e5ff29e82fae9d1cdca56dd81b281fc8d14e
Takuma Kajikawa <kj1ktk@gmail.com>
2022-08-01 21:11:55 +0900

    Change accessor of GetArticles to private

--- whatday/whatday_test.go
+++ whatday/whatday_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func TestNewArticles(t *testing.T) {
+func TestWhatDay(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	date := time.Date(2018, 2, 22, 0, 0, 0, 0, time.Local)
 	client := NewMockClient(ctrl)
@@ -18,7 +18,7 @@ func TestNewArticles(t *testing.T) {
 			Text:  "２月２２日を猫の鳴き声「ニャン・ニャン・ニャン」ともじって決められた日で、猫の日制定委員会が１９８７年（昭和６２年）に制定した。",
 			Url:   EndPoint + "yurai_other.php?MD=4&NM=10088",
 		}, nil)
-	articles, err := whatday{client: client}.GetArticles(date)
+	articles, err := whatday{client: client}.getArticles(date)
 	if err != nil {
 		t.Error(err)
 	}
